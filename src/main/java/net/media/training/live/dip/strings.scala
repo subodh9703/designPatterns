0b0857944a997e9898a8de94553b83f812727063
subodh kumar <subodh.ku@media.net>
2024-07-25 16:02:16 +0530

    original files

---
+++ src/main/java/net/media/training/live/dip/strings.scala
@@ -0,0 +1,7 @@
+package net.media.training.live.dip;
+
+object strings{
+  final val INPUT_FILE = "/Users/goyalamit/Sandbox/training/src/solid_2011/live/dip/beforeEncryption.txt"
+  final val OUTPUT_FILE = "/Users/goyalamit/Sandbox/training/src/solid_2011/live/dip/afterEncryption.txt"
+  final val HOST_NAME = "myfirstappwith.appspot.com"
+}
